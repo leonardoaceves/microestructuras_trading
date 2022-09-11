b015fc86bb8ba176f820340852aa967b607aee9f
José Leonardo Aceves González <47115002+leonardoaceves@users.noreply.github.com>
2022-09-11 15:50:44 -0500

    Correcciones

---
+++ data.py
@@ -0,0 +1,20 @@
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import yfinance as yf
+import warnings
+warnings.filterwarnings("ignore")
+
+comision, capital = 0.00125, 1000000
+naftrac = pd.read_excel('C:\\naftrac_data.xlsx', index_col='Fecha')
+naftrac.columns = naftrac.columns.str.upper()
+prices = yf.download(naftrac.columns.tolist(), start='2020-01-31', end='2022-07-30')
+prices = pd.DataFrame(prices.Close).dropna(axis = 1)
+pricesint = prices.loc[prices.index.intersection(naftrac.index.unique())]
+pricesret = pricesint.pct_change().apply(lambda x: np.log(1+x)).dropna()
+df_activa = pd.DataFrame()
+
+todropp = ['ALPEKA.MX', 'BSMXB.MX', 'GCC.MX', 'GENTERA.MX', 'IENOVA.MX', 'NEMAKA.MX', 'Q.MX', 'RA.MX', 'VESTA.MX', 'VOLARA.MX']
+todropn = ['MXN.MX']
